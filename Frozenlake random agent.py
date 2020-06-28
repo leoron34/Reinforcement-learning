a572fafb35726a4ffedfaadefe0b16f854e3c596
Ronit <41695842+leoron34@users.noreply.github.com>
2020-06-28 19:25:40 +0530

    Create Frozenlake random agent.py

---
+++ Frozenlake random agent.py
@@ -0,0 +1,55 @@
+import gym
+
+
+def agent():
+    # Starting new environment
+    env = gym.make('FrozenLake-v0')
+
+    # Variable to hold total number of steps over all the episodes
+    # This is required in the end to calculate average number of steps
+    # over all the episodes
+    total_steps = []
+    num_episodes = 50
+
+    for i in range(num_episodes):
+        #  As this is a fresh start of an episode,
+        #  use reset to start new episode in environment
+        env.reset()
+
+        # steps - variable to store number of steps we survive
+        # in this episode
+        steps = 0
+
+        #  Try until is episode is finished
+        while True:
+            steps += 1
+
+            #  Select any random action
+            action = env.action_space.sample()
+
+            #  Take that action on environment
+            #  and get new state and rewards
+            new_state, reward, done, info = env.step(action)
+
+            print(new_state)
+            print(info)
+
+            # Render the current state of the env
+            env.render()
+
+            # Gym environment will return done = True
+            # if episode is finished
+            if done:
+                total_steps.append(steps)
+                print("Episode finished after",steps,"steps")
+                break
+
+    # Close the environment
+    env.close()
+    env.env.close()             #not compulsorily required
+
+    #  Calculate average number of steps we survived for all the episodes
+    print("Average Number of Steps:{0}".format(sum(total_steps) / num_episodes))
+
+
+agent()
